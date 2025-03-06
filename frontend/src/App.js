0a041542141803793eed2bea62d7a83a6d92099e
Truongjava <thanhtruong16042004@gmail.com>
2025-03-06 14:37:55 +0700

    update images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import './App.css';
-import logo from './logohus.jpg';
+import logo from './logohuss.jpg';
 
 function App() {
     const [message, setMessage] = useState("");
